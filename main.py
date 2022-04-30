0529ed1d613047a26abbacd51cd6dda07e84dc0d
toscha3030 <aukmiit@gmail.com>
2022-04-30 14:27:12 +0200

    line, not line, not line 2, sphere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
-import math
+from math import asin, cos, sin, atan
+from math import pi
 import sys
 import pygame
 
@@ -19,6 +20,7 @@ color_axes = '#000000'
 color_line = '#374534'
 color_sin = '#ad1111'
 color_arcsin = '#2311ad'
+color_sphere = '#37eb34'
 
 canvas_height = 800
 canvas_width = 800
@@ -31,8 +33,8 @@ clock = pygame.time.Clock()
 
 
 class GeometryObject:
-    def __init__(self, fild_or_cylinder, row, step, height, diameter, line_notLine_notLine2):
-        self.fild_or_cylinder = fild_or_cylinder
+    def __init__(self, field_or_cylinder, row, step, height, diameter, line_notLine_notLine2):
+        self.field_or_cylinder = field_or_cylinder
         self.row = row
         self.step = step
         self.height = height
@@ -41,9 +43,10 @@ class GeometryObject:
         self.coordinate_x = [[0] * (self.row + 1) for i in range(2 * (self.row + 1))]
         self.coordinate_y = [[0] * (self.row + 1) for i in range(2 * (self.row + 1))]
         self.coordinate_z = [[0] * (self.row + 1) for i in range(2 * (self.row + 1))]
+        self.radius_of_cylinder = self.row * self.step / (2 * pi)
 
         # make coordinate
-        if self.fild_or_cylinder == "fild":
+        if self.field_or_cylinder == "field":
             self.make_field()
         else:
             self.make_cylinder()
@@ -53,6 +56,10 @@ class GeometryObject:
             self.make_2d_line()
         elif self.line_notLine_notLine2 == "notLine":
             self.make_2d_notLine()
+        elif self.line_notLine_notLine2 == "sphere":
+            self.make_2d_sphere()
+        elif self.line_notLine_notLine2 == "2atan":
+            self.make_2d_2atan()
         else:
             self.make_2d_notLine2()
 
@@ -64,6 +71,10 @@ class GeometryObject:
             self.make_2d_line()
         elif self.line_notLine_notLine2 == "notLine":
             self.make_2d_notLine()
+        elif self.line_notLine_notLine2 == "sphere":
+            self.make_2d_sphere()
+        elif self.line_notLine_notLine2 == "2atan":
+            self.make_2d_2atan()
         else:
             self.make_2d_notLine2()
 
@@ -126,12 +137,61 @@ class GeometryObject:
                 else:
                     z = self.coordinate_z[k][i]
                 x = self.coordinate_x[k][i]
-                x1 = self.diameter * math.asin(x / (z ** 2 + x ** 2)**0.5)
+                x1 = self.diameter * asin(x / (z ** 2 + x ** 2) ** 0.5)
                 y = self.coordinate_y[k][i]
-                y1 = self.diameter * math.asin(y / (z ** 2 + y ** 2)**0.5)
-                #if abs(y / z) <= 1 and abs(x / z) <= 1:
+                y1 = self.diameter * asin(y / (z ** 2 + y ** 2) ** 0.5)
                 x2d.append(x1)
                 y2d.append(y1)
+
+            if len(x2d) >= 2:
+                self.draw_a_line_from_array(x2d, y2d, color)
+            x2d = []
+            y2d = []
+
+    def make_2d_sphere(self):
+        color = color_sphere
+        x2d = []
+        y2d = []
+        for k in range(0, 2 * (self.row + 1)):
+            for i in range(0, (self.row + 1)):
+                if self.coordinate_z[k][i] <= 0:
+                    z = 0.01
+                else:
+                    z = self.coordinate_z[k][i]
+                x = self.coordinate_x[k][i]
+                y = self.coordinate_y[k][i]
+                l = self.diameter * atan(y / (z ** 2 + x ** 2) ** 0.5)
+
+                x = self.diameter * atan(x / z)
+                y = l
+
+                x2d.append(x)
+                y2d.append(y)
+
+            if len(x2d) >= 2:
+                self.draw_a_line_from_array(x2d, y2d, color)
+            x2d = []
+            y2d = []
+
+    def make_2d_2atan(self):
+        color = color_sphere
+        x2d = []
+        y2d = []
+        for k in range(0, 2 * (self.row + 1)):
+            for i in range(0, (self.row + 1)):
+                if self.coordinate_z[k][i] <= 0:
+                    z = 0.01
+                else:
+                    z = self.coordinate_z[k][i]
+                x = self.coordinate_x[k][i]
+                y = self.coordinate_y[k][i]
+                l = self.diameter * atan(z / (x ** 2 + y ** 2) ** 0.5)
+                alpha = atan(x / y)
+                x = l * cos(alpha)
+                y = l * sin(alpha)
+
+                x2d.append(x)
+                y2d.append(y)
 
             if len(x2d) >= 2:
                 self.draw_a_line_from_array(x2d, y2d, color)
@@ -176,11 +236,35 @@ class GeometryObject:
                              (x1 + x0, y1 + y0),
                              (x2 + x0, y2 + y0),
                              )
-            x1 = x2
-            y1 = y2
+            x1, y1 = x2, y2
 
     def make_cylinder(self):
-        pass
+        k = 0
+        # make vertical lines (direction z)
+        d_alpha = 2 * pi / self.row
+        alpha = 0
+        for i in range(0, self.row + 1):
+            for j in range(0, self.row + 1):
+                x = self.radius_of_cylinder * cos(i * d_alpha)
+                y = self.radius_of_cylinder * sin(i * d_alpha) + self.radius_of_cylinder - self.height
+                z = j * self.step
+                self.coordinate_x[k][j] = x
+                self.coordinate_y[k][j] = y
+                self.coordinate_z[k][j] = z
+            k += 1
+
+        # make horizontal lines (direction x)
+        for j in range(0, self.row + 1):
+            t = 0
+            for i in range(0, self.row + 1):
+                x = self.radius_of_cylinder * cos(i * d_alpha)
+                y = self.radius_of_cylinder * sin(i * d_alpha) + self.radius_of_cylinder - self.height
+                z = j * self.step
+                self.coordinate_x[k][t] = x
+                self.coordinate_y[k][t] = y
+                self.coordinate_z[k][t] = z
+                t += 1
+            k += 1
 
 
 def draw_axes():
@@ -200,49 +284,58 @@ def main_loop_start():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     screen.fill(WHITE)
-                    fild_line.move_the_object(0, step)
-                    fild_not_line.move_the_object(0, step)
-                    fild_not_line2.move_the_object(0, step)
+                    for object_i in group_to_draw:
+                        object_i.move_the_object(0, step)
                     draw_axes()
                 elif event.key == pygame.K_UP:
                     screen.fill(WHITE)
-                    fild_line.move_the_object(0, -step)
-                    fild_not_line.move_the_object(0, -step)
-                    fild_not_line2.move_the_object(0, -step)
+                    for object_i in group_to_draw:
+                        object_i.move_the_object(0, -step)
                     draw_axes()
                 elif event.key == pygame.K_LEFT:
                     screen.fill(WHITE)
-                    fild_line.move_the_object(-step, 0)
-                    fild_not_line.move_the_object(-step, 0)
-                    fild_not_line2.move_the_object(-step, 0)
+                    for object_i in group_to_draw:
+                        object_i.move_the_object(-step, 0)
                     draw_axes()
                 elif event.key == pygame.K_RIGHT:
                     screen.fill(WHITE)
-                    fild_line.move_the_object(step, 0)
-                    fild_not_line.move_the_object(step, 0)
-                    fild_not_line2.move_the_object(step, 0)
+                    for object_i in group_to_draw:
+                        object_i.move_the_object(step, 0)
                     draw_axes()
 
         pygame.display.flip()  # draw all the things
-        clock.tick(FPS)  # neu iteration
+        clock.tick(FPS)  # new iteration
 
 
 if __name__ == '__main__':
     diameter = canvas_width
     height = 2
-    n = 100
+    n = 20
     step = 1
     pygame.display.set_caption("Perspective")
     screen.fill(WHITE)
 
-    fild_line = GeometryObject(
-        "fild", n, step, height, diameter, "line")
-    # (fild_or_cylinder, row, step, height, radius, line_notLine_notLine2)
-    fild_not_line = GeometryObject(
-        "fild", n, step, height, diameter, "notLine")
-    # (fild_or_cylinder, row, step, height, radius, line_notLine_notLine2)
-    fild_not_line2 = GeometryObject(
-        "fild", n, step, height, diameter, "notLine2")
-    # (fild_or_cylinder, row, step, height, radius, line_notLine_notLine2)
+    to_draw = "field"
+
+    if to_draw == "cylinder":
+        field_line = GeometryObject(
+            "field", n, step, height, diameter, "line")
+        field_not_line = GeometryObject(
+            "field", n, step, height, diameter, "line")
+        field_not_line2 = GeometryObject(
+            "field", n, step, height, diameter, "2atan")
+
+        group_of_fields = [field_line, field_not_line, field_not_line2]
+        group_to_draw = group_of_fields
+    else:
+        cylinder_line = GeometryObject(
+            "cylinder", n, step, height, diameter, "line")
+        cylinder_not_line = GeometryObject(
+            "cylinder", n, step, height, diameter, "2atan")
+        cylinder_not_line2 = GeometryObject(
+            "cylinder", n, step, height, diameter, "line")
+        group_of_cylinders = [cylinder_line, cylinder_not_line2, cylinder_not_line]
+        group_to_draw = group_of_cylinders
+
     draw_axes()
     main_loop_start()
